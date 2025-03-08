c6b146de7a7dc6a2976596e114a6b12616fbb463
Daniel Savio <daniel.savio@treetech.com.br>
2025-03-08 10:30:13 -0300

    Resolving the api url

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,7 @@
 export const variables = {
-    api: "http://localhost:3333",
-    //api: "https://ml-project-pxid.onrender.com",
+    //api: "http://localhost:3333",
+    api: "https://ml-project-pxid.onrender.com",
     discord: "https://discord.gg/D7B6EEnuHs",
-    default: "http://localhost:5173",
-    //default: "",
+    //default: "http://localhost:5173",
+    default: "",
 }
